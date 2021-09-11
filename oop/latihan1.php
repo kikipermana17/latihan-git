7341cb7a6a45d62d0ab1d8d75b359731340f9f27
kikipermana17 <kiw170904@gmail.com>
2021-09-11 00:53:17 -0700

    mengubah isi file

--- oop/latihan1.php
+++ oop/latihan1.php
@@ -17,11 +17,26 @@ class komputer{
     }
 }
 
-$komputer1 = new komputer();
-echo "Nama Pemilik : " . $komputer1->nama ."<br>";
-echo "Merek Komputer : " . $komputer1->merk ."<hr>";
+        $komputer1 = new komputer();
+        echo "Nama Pemilik : " . $komputer1->nama ."<br>";
+        echo "Merek Komputer : " . $komputer1->merk ."<hr>";
 
-$komputer2 = new komputer();
-echo "Nama Pemilik : " . $komputer1->nama ."<br>";
-echo " Komputer : " . $komputer1->menyala() ."<hr>";
+        $komputer2 = new komputer();
+        echo "Nama Pemilik : " . $komputer2->nama ."<br>";
+        echo " Komputer : " . $komputer2->menyala() ."<hr>";
+
+        $komputer3 = new komputer();
+        $komputer3->merk = "Philips";
+        echo "Merk Komputer : " . $komputer3->merk ."<br>";
+        echo " Komputer : " . $komputer3->matikan() ."<hr>";
+
+        $komputer4 = new komputer();
+        $komputer4->nama = "Agus";
+        echo "Nama Komputer : " . $komputer4->nama ."<br>";
+        echo "Ukuran Komputer : " . $komputer4->ukuran ."<hr>";
+
+        $komputer5 = new komputer();
+        $komputer5->ukuran = "7inc";
+        echo "Merek Komputer : " . $komputer5->merk ."<br>";
+        echo "Ukuran Komputer : " . $komputer5->ukuran ."<hr>";
 ?>
