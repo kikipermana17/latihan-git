0e22d6d994f97babb86a24f88452c7e6e9190da2
kikipermana17 <kiw170904@gmail.com>
2021-09-11 00:21:07 -0700

    menambahkan file

---
+++ oop/oop1.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class kucing{
+    public $warna = "coklat";
+    public $jumlahkaki = 4;
+    public $jenisBulu = "Panjang";
+    public $makananFav = "Ikan Asin";
+
+    public function bersuara()
+    {
+        return "Meowng ... meowng ... meowng";
+    }
+
+    public function berburu()
+    {
+        return " Berburu Ikan";
+    }
+}
